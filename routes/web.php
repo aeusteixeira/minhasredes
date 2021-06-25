1549e6ff2cb3bba205b4d9d082c122952745c569
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2021-06-25 20:50:25 -0300

    Finalizado a estruturação das rotas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
+use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [WebController::class, 'index']);
+Route::prefix(['prefix' => 'admin', 'as' => 'admin'])->group(function () {
+    Route::get('/', [WebController::class, 'dashboard'])->name('index');
+    Route::get('/login', [WebController::class, 'login'])->name('login.index');
+    Route::get('/logout', [WebController::class, 'logout'])->name('logout');
+    Route::post('/login', [WebController::class, 'enterLogin'])->name('login');
+    Route::get('/settings')->name('settings');
 });
