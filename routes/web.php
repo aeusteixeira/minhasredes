97b26a1d2cbf7e71134c58e5d182bfa2b64b514d
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2021-06-25 20:59:16 -0300

    Corrigido rotas

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [WebController::class, 'index']);
-Route::prefix(['prefix' => 'admin', 'as' => 'admin'])->group(function () {
+Route::prefix('admin', ['as' => 'admin'])->group(function () {
     Route::get('/', [WebController::class, 'dashboard'])->name('index');
     Route::get('/login', [WebController::class, 'login'])->name('login.index');
     Route::get('/logout', [WebController::class, 'logout'])->name('logout');
