1fb35245bc8f7f56bb7786ffad3418516cc21ef8
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2021-06-26 19:38:34 -0300

    Configurado models

--- app/Models/Network.php
+++ app/Models/Network.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Network extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'icon',
+        'background_color',
+        'icon_color',
+    ];
 }
