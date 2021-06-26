78b1db2299f83e6be85ef2190f75f26044cd7727
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2021-06-25 21:12:14 -0300

    Criado base da view de login

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [WebController::class, 'index']);
+Route::get('/login', [WebController::class, 'login'])->name('login.index');
+Route::post('/login', [WebController::class, 'enterLogin'])->name('login');
+
 Route::prefix('admin', ['as' => 'admin'])->group(function () {
     Route::get('/', [WebController::class, 'dashboard'])->name('index');
-    Route::get('/login', [WebController::class, 'login'])->name('login.index');
     Route::get('/logout', [WebController::class, 'logout'])->name('logout');
-    Route::post('/login', [WebController::class, 'enterLogin'])->name('login');
     Route::get('/settings')->name('settings');
 });
