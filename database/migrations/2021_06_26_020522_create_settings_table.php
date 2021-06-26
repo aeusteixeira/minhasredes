aa38125b0333c7d2fc0f58c3118f45b708518bad
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2021-06-25 23:10:50 -0300

    Finalizado criação e configuração das migrations

--- database/migrations/2021_06_26_020522_create_settings_table.php
+++ database/migrations/2021_06_26_020522_create_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateSettingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('settings', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->text('description')->nullable();
+            $table->string('background')->default('#f1f1f1');
+            $table->string('banner')->nullable();
+            $table->string('banner_link')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('settings');
     }
 }
