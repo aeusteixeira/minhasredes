d53997d9b2fd260b31556bbd9a271ef8ae2bd4f0
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2021-06-25 21:44:27 -0300

    Ajustado agrupamento de rotas

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', [WebController::class, 'index']);
 Route::get('/login', [WebController::class, 'login'])->name('login.index');
 Route::post('/login', [WebController::class, 'enterLogin'])->name('login');
 
-Route::prefix('admin', ['as' => 'admin'])->group(function () {
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::get('/', [WebController::class, 'dashboard'])->name('index');
     Route::get('/logout', [WebController::class, 'logout'])->name('logout');
     Route::get('/settings')->name('settings');
